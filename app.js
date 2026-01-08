d1d91cd9c684e04a6b4d39a97bb381eb70cc6db6
devkhera24 <kheradev4@gmail.com>
2026-01-08 11:10:03 +0530

    Connecting changes to app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import { initEventCollector } from "./core/eventCollector.js";
-import { pushEvent } from "./core/eventBuffer.js";
+import { pushEvent, startBufferProcessor } from "./core/eventBuffer.js";
+import { analyzeEventBatch } from "./core/analyzer.js";
 
 console.log("CogniScope initialized");
 
 initEventCollector((event) => {
   pushEvent(event);
+});
+
+startBufferProcessor((eventBatch) => {
+  analyzeEventBatch(eventBatch);
 });
