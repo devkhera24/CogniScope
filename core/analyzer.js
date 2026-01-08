9510780a54b2adb3c5a0243f9e7faf429918fba4
devkhera24 <kheradev4@gmail.com>
2026-01-08 11:09:03 +0530

    Added log check for analyzer

---
+++ core/analyzer.js
@@ -0,0 +1,8 @@
+// core/analyzer.js
+
+export function analyzeEventBatch(events) {
+  // Day 3: No analytics yet
+  console.log("Analyzing batch:", events.length, "events");
+
+  // This will later return metrics
+}
